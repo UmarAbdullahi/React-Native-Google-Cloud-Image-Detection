45821851758c3d65380695c0b851cf0947bb671b
Umar Abdullahi <u.abdullahi001@gmail.com>
2019-03-29 08:10:17 +0100

    Update environment.js

--- frontend/src/config/environment.js
+++ frontend/src/config/environment.js
@@ -1,12 +1,12 @@
 var environments = {
 	staging: {
-		FIREBASE_API_KEY: 'AIzaSyCjQbSr3bahzlZdi6VSeSLXn-4CJ4abNiI',
-		FIREBASE_AUTH_DOMAIN: 'logo-detect-76b90.firebaseapp.com',
-		FIREBASE_DATABASE_URL: 'https://logo-detect-76b90.firebaseio.com',
-		FIREBASE_PROJECT_ID: 'logo-detect-76b90',
-		FIREBASE_STORAGE_BUCKET: 'logo-detect-76b90.appspot.com',
-		FIREBASE_MESSAGING_SENDER_ID: '616240319810',
-		GOOGLE_CLOUD_VISION_API_KEY: 'AIzaSyAQK_LjA5EJwP5O0UmvzGIwLoVTc5IhtHw'
+		FIREBASE_API_KEY: 'XXXXX',
+		FIREBASE_AUTH_DOMAIN: 'XXXX',
+		FIREBASE_DATABASE_URL: 'XXXXX',
+		FIREBASE_PROJECT_ID: 'XXXXX',
+		FIREBASE_STORAGE_BUCKET: 'XXXXX',
+		FIREBASE_MESSAGING_SENDER_ID: 'XXXXXX',
+		GOOGLE_CLOUD_VISION_API_KEY: 'XXXXXXXXX'
 	},
 	production: {
 		// Warning: This file still gets included in your native binary and is not a secure way to store secrets if you build for the app stores. Details: https://github.com/expo/expo/issues/83
